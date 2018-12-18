e6d717d382e9230f130c3e8b362f6fcc51b6f9c6
Sergey Kryukov <cydkom@gmail.com>
2018-12-18 21:38:32 +0300

    to add Input and Output

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+# Madlibs - Practice Input and Output
+var_verb = input(Please enter a verb)
+var_noun = input(Please enter a none)
+var_adjective = input(Please enter a adjective)
+
+# Template:
+#     I enjoy practice! I find it help me to __(verb)__better
+#     Without practice, my __(none)___ would probably not even work.
+#     My code is getting more __(adjective)__ every single day!
+
+
+
+# TODO: Prompt the user for parts of speach and store  them in the  variables
+
+
+
+
+
+# TODO:  Output the temple to the screen the blanks filled out with what the user stated
