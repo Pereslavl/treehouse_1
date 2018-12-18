65d3e8ef82758c2345dfa4c7ca88cc50b01e7b09
Sergey Kryukov <45299453+Pereslavl@users.noreply.github.com>
2018-12-18 20:15:45 +0300

    Create treehouse.py

---
+++ treehouse.py
@@ -0,0 +1,9 @@
+first_name = input("Whaat is your name?")
+print("Hello,", first_name)
+if first_name == "Sergey":
+    print(first_name, "is learning Python")
+elif first_name == "Vika":
+    print(first_name, "you too learning Python")
+else:
+    print("You should totally learn Python, {}!".formate(first_name))
+    print("Have a nice day {}!".format(first_name))
