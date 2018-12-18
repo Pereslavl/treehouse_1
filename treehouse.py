7b9a4f0179a34831007328ff3f5f6f601196042d
Sergey Kryukov <cydkom@gmail.com>
2018-12-18 20:48:12 +0300

    mood change on the bad

--- treehouse.py
+++ treehouse.py
@@ -6,4 +6,4 @@ elif first_name == "Vika":
     print(first_name, "you too learning Python")
 else:
     print("You should totally learn Python, {}!".formate(first_name))
-    print("Have a nice day {}!".format(first_name))
+    print("Have a bad day {}!".format(first_name))
